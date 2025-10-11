03767781d8cbbaea4df30f98b1fc7ec8342b7f75
misa <mischa.zeu1998@gmail.com>
2025-10-11 22:31:13 +0900

    e-mail added

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -5,7 +5,9 @@ import javax.persistence.*;
 @Entity
 @Table(name = "users")
 public class User {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, length = 64)
@@ -14,14 +16,53 @@ public class User {
     @Column(nullable = false, length = 64)
     private String lastName;
 
+    @Column(nullable = false, unique = true, length = 255)
+    private String email;
+
     public User() { }
+
     public User(String firstName, String lastName) {
-        this.firstName = firstName; this.lastName = lastName;
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
-    public Long getId() { return id; }
-    public String getFirstName() { return firstName; }
-    public void setFirstName(String firstName) { this.firstName = firstName; }
-    public String getLastName() { return lastName; }
-    public void setLastName(String lastName) { this.lastName = lastName; }
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    // getters / setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
